122f05e7b44970130ee4ffe0f530962c6d12af4f
alexluka1 <101130210+alexluka1@users.noreply.github.com>
2022-09-14 14:41:30 +0100

    Added base template of blog app Added form to add to blog. Add <div> to enter blog posts into

--- blog-app/src/App.svelte
+++ blog-app/src/App.svelte
@@ -1,7 +1,58 @@
 <script>
+  import { each, prevent_default } from "svelte/internal";
+
+  import Article from "./lib/Article.svelte";
+
+  let header;
+  let body;
+
+  let headerArr =
+    JSON.parse(localStorage.getItem("headerArr")) != null
+      ? JSON.parse(localStorage.getItem("headerArr"))
+      : [];
+
+  let bodyArr =
+    JSON.parse(localStorage.getItem("bodyArr")) != null
+      ? JSON.parse(localStorage.getItem("bodyArr"))
+      : [];
+
+  const submitBlog = (header, body) => {
+    headerArr = [...headerArr, header];
+    bodyArr = [...bodyArr, body];
+    localStorage.setItem("headerArr", JSON.stringify(headerArr));
+    localStorage.setItem("bodyArr", JSON.stringify(bodyArr));
+  };
 </script>
 
-<main />
+<main>
+  <div class="container">
+    <div class="left-bar">
+      <h1>Blog App</h1>
+
+      <form action="">
+        <input type="text" bind:value={header} />
+        <input type="text" bind:value={body} />
+        <button on:click={() => submitBlog(header, body)}>Submit Post</button>
+      </form>
+    </div>
+    <div class="right-bar">
+      {#each headerArr as head, index}
+        <Article header={headerArr[index]} body={bodyArr[index]} />
+      {/each}
+    </div>
+  </div>
+</main>
 
 <style>
+  .container {
+    display: grid;
+    grid-template-columns: 1fr 4fr;
+    gap: 2em;
+    margin: 2em;
+  }
+
+  .right-bar {
+    display: grid;
+    gap: 1em;
+  }
 </style>
---
+++ blog-app/src/lib/Article.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let header;
+  export let body;
+</script>
+
+<main>
+  <article>
+    <h3>{header}</h3>
+    <p>{body}</p>
+  </article>
+</main>
+
+<style>
+  article {
+    border: black 1px solid;
+    padding: 1em;
+    min-width: 300px;
+  }
+  h3 {
+    margin-top: -0.8em;
+  }
+</style>
